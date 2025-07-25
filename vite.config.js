26cb46b4c5f56cc97ce83c26a6a8ceae599a36a8
Alexandre Guenot <alexandreguenot@MacBook-Air-de-Alexandre.local>
2025-07-25 19:49:49 +0200

    Fix: Externalize react-select for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: true,
     rollupOptions: {
-      onwarn(warning, warn) {
-        // Ignore certain warnings or log them
-        console.warn('⚠️ Vite build warning:', warning.message);
-        warn(warning);
-      },
+      external: ['react-select'], // <-- Ajoute ici tous les modules problématiques
     }
   }
-});
+})
